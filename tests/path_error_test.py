a0d8ed92befe469ae119612e7cec29bab20d13ce
Rafael M Mudafort <rafmudaf@gmail.com>
2024-11-18 11:39:07 -0600

    Add test for carrot in path

--- tests/path_error_test.py
+++ tests/path_error_test.py
@@ -2,13 +2,21 @@
 from win_path.class_with_path import ClassWithPath
 
 def test_valid_path_that_does_exists():
-    error = ClassWithPath("C:/Users")
-    assert error.path_exists() == True
+    path_class = ClassWithPath("C:/Users")
+    print(path_class.path_string)
+    assert path_class.path_exists() == True
 
 def test_valid_path_that_does_not_exist():
-    error = ClassWithPath("C:/Users/Invalid")
-    assert error.path_exists() == False
+    path_class = ClassWithPath("C:/Users/Invalid")
+    print(path_class.path_string)
+    assert path_class.path_exists() == False
 
-def test_invalid_path():
-    error = ClassWithPath("C:/Users/*/")
-    assert error.path_exists() == False
+def test_invalid_path_asterisk():
+    path_class = ClassWithPath("C:/Users/*/")
+    print(path_class.path_string)
+    assert path_class.path_exists() == False
+
+def test_invalid_path_carrot():
+    path_class = ClassWithPath("<some text>")
+    print(path_class.path_string)
+    assert path_class.path_exists() == False
